5d2c62fdc06f785e19cf8fb7a24eb8616af78d83
AndriyCO <Aolijnyk73@gmail.com>
2017-07-22 02:15:11 +0300

    second comit in main line

--- ProgectCreatingInEclipse9/src/pcie_package/Main.java
+++ ProgectCreatingInEclipse9/src/pcie_package/Main.java
@@ -3,7 +3,7 @@ package pcie_package;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Changes in the project for the master-branch of the project");
 
 	}
 
